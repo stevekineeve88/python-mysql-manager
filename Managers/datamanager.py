934f6a866c47c4910b2f017263dc5d046d659ec9
stevekineeve88 <50559860+stevekineeve88@users.noreply.github.com>
2020-10-20 13:28:23 -0400

    Initial commit

---
+++ Managers/datamanager.py
@@ -0,0 +1,54 @@
+import mysql.connector
+
+from objects.datacontainer import DataContainer
+
+# Manages MySQL queries to database with container result
+
+
+class DataManager:
+    __SELECT = "Select"
+    __INSERT = "Insert"
+    __UPDATE = "Update"
+    __DELETE = "Delete"
+    __QUERY = "Query"
+
+    def __init__(self, config):
+        self.__mysql = mysql.connector.connect(
+            host=config["host"],
+            port=config["port"],
+            user=config["user"],
+            password=config["password"],
+            database=config["database"]
+        )
+
+    def select(self, sql: str, values: tuple = ()) -> DataContainer:
+        return self.__query(sql, values, self.__SELECT)
+
+    def insert(self, sql: str, values: tuple = ()) -> DataContainer:
+        return self.__query(sql, values, self.__INSERT)
+
+    def update(self, sql: str, values: tuple = ()) -> DataContainer:
+        return self.__query(sql, values, self.__UPDATE)
+
+    def delete(self, sql: str, values: tuple = ()) -> DataContainer:
+        return self.__query(sql, values, self.__DELETE)
+
+    def getConnection(self) -> mysql.connector:
+        return self.__mysql
+
+    def __query(self, sql: str, values: tuple, query: str = __QUERY) -> DataContainer:
+        cursor = self.__mysql.cursor()
+        try:
+            cursor.execute(sql, values)
+            if query != self.__SELECT:
+                self.__mysql.commit()
+            container = DataContainer(True)
+            if query == self.__SELECT:
+                container.setData(cursor.fetchall())
+            if query == self.__INSERT:
+                container.setLastInsertId(cursor.lastrowid)
+            cursor.close()
+            return container
+        except mysql.connector.Error as err:
+            cursor.close()
+            return DataContainer(False, err.msg, err.errno)
---
+++ objects/datacontainer.py
@@ -0,0 +1,31 @@
+
+
+class DataContainer:
+
+    def __init__(self, status: bool, message: str = "", code: int = 200):
+        self.status = status
+        self.message = message
+        self.code = code
+        self.data = []
+        self.lastInsertId = None
+
+    def setData(self, data: list):
+        self.data = data
+
+    def getData(self) -> list:
+        return self.data
+
+    def setLastInsertId(self, lastInsertId):
+        self.lastInsertId = lastInsertId
+
+    def getLastInsertId(self):
+        return self.lastInsertId
+
+    def getStatus(self) -> bool:
+        return self.status
+
+    def getMessage(self) -> str:
+        return self.message
+
+    def getCode(self) -> int:
+        return self.code
