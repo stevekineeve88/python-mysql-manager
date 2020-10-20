cb56b60ddca1cd4e1754739da1c904f8710a22e7
Stephen Ayre <stevemamajama@gmail.com>
2020-10-20 14:49:03 -0400

    setup file

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="python-mysql-manager",
+    version="1.0.0.0",
+    author="Stephen Ayre",
+    author_email="stevemamajama@gmail.com",
+    description="A wrapper for simplifying python mysql operations",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/stevekineeve88/python-mysql-manager.git",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.8'
+)
